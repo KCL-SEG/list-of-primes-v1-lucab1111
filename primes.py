76e0949b9f7246f93486034ebaba5f2815abde33
Luca Boccardo <luca.boccardo@kcl.ac.uk>
2022-10-26 23:23:04 +0100

    Finish primes.py code

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    i = 1
+    for x in range(0, number_of_primes):
+        prime_found = False
+
+        while prime_found == False:
+            i += 1
+            prime_found = True
+            for x in range(2, int(i**0.5) + 1):
+                if i % x == 0:
+                    prime_found = False
+                    break
+        list.append(i)
+
     return list
